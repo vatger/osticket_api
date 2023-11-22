85f9de888d5d2f3990d5f2d3b35e034031a7509b
Sebastian Klietz <mail@sklietz.de>
2023-11-22 15:11:18 +0100

    make code a little prettier

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,14 +2,7 @@ import { Request, Response} from "express";
 import {sequelizeHost} from "../core/Sequelize";
 import {QueryTypes} from "sequelize";
 import {Config} from "../core/Config";
-
-type OstStaffId = {
-    staff_id: number
-}
-
-type OstDeptId = {
-    dept_id: number
-}
+import {sql} from "../utility/SQLStatements";
 
 type RequestData = {
     user_id: string
@@ -18,9 +11,17 @@ type RequestData = {
     lastname:string
     email:string
 }
-type UserCreation = {
+
+type User = {
+    username: string
     staff_id: number
+    isactive: number
     pwd: string
+    created: boolean
+}
+
+type OstDeptId = {
+    dept_id: number
 }
 
 /**
@@ -31,105 +32,130 @@ type UserCreation = {
 
 async function syncUserGroups(request: Request, response: Response): Promise<void>{
 
-    let userCreated = false;
-    let NewUser: UserCreation = {staff_id: 0, pwd: ""};
+    let user: User = {  username: "",
+                        isactive: 0,
+                        pwd: "",
+                        created: false,
+                        staff_id: 0};
+
     const requestData: RequestData = request.body;
 
+    // no user id in request
     if ( requestData?.user_id == null || requestData?.user_id == ""  )
     {
         response.status(500).send({error: "user_id empty"});
         return;
     }
 
-
-    const sql_find_user: string = `SELECT * FROM ost_staff WHERE username=?`;
-    const sql_find_groups: string = `SELECT dept_id FROM ost_staff_dept_access WHERE staff_id=?`;
-    const sql_delete_roles: string = `DELETE FROM ost_staff_dept_access WHERE staff_id=? and dept_id=?`;
-    const sql_insert_roles: string = `INSERT INTO ost_staff_dept_access (staff_id, dept_id, role_id, flags) VALUES (?, ?, ?, ?)`;
-
-    const data : OstStaffId[] = await sequelizeHost.query(sql_find_user, {
+    // find user in osticket table
+    const users : User[] = await sequelizeHost.query(sql.find_user, {
         type: QueryTypes.SELECT,
         replacements: ["v".concat(requestData.user_id)],
     });
-    let StaffId: number;
 
-    if (data.length != 0) {
-        StaffId = data[0].staff_id;
+
+    if (users.length != 0) {
+        // user found => assign
+        user = users[0];
     } else {
+
         if (requestData.dept_ids.length != 0) {
-            NewUser = await createUser(requestData);
-            if (NewUser.staff_id == 0) {
+            // depts assigned => create user
+            user = await createUser(requestData);
+            if (user.staff_id == 0) {
+                // user could not be created
                 response.status(500).send({error: "user could not be created"});
                 return;
             } else {
-                userCreated = true;
-                StaffId = NewUser.staff_id
+                user.created = true;
             }
         }else{
+            // no depts assigned => return
             response.status(200).send({error: "Empty Groups - no user created"});
             return ;
         }
     }
 
-    const DBCurrentGroups : OstDeptId[] = await sequelizeHost.query(sql_find_groups, {
+    // User is inactive and groups assigned => activate user
+    if( user.isactive == 0 && requestData.dept_ids.length != 0)
+    {
+        await sequelizeHost.query(sql.switch_active, {
+            type: QueryTypes.UPDATE,
+            replacements: [1, user.staff_id]});
+    }
+    // User is active and no groups assigned => inactivate user
+    if( user.isactive == 1 && requestData.dept_ids.length == 0)
+    {
+        await sequelizeHost.query(sql.switch_active, {
+            type: QueryTypes.UPDATE,
+            replacements: [0, user.staff_id]});
+    }
+
+    // select current department assignments
+    const DBCurrentGroups : OstDeptId[] = await sequelizeHost.query(sql.find_groups, {
         type: QueryTypes.SELECT,
-        replacements: [StaffId],
+        replacements: [user.staff_id],
     });
+
+    // intersect currently assigned with new assigned depts
     const CurrentGroups = DBCurrentGroups.map(d=>d.dept_id);
     const NewGroups = requestData.dept_ids;
-
     const GroupsAdd = NewGroups.filter(x => !CurrentGroups.includes(x));
     const GroupsDel = CurrentGroups.filter(x => !NewGroups.includes(x));
 
+    // delete groups
     for (const dept_id of GroupsDel) {
-        await sequelizeHost.query(sql_delete_roles, {
+        await sequelizeHost.query(sql.delete_roles, {
             type: QueryTypes.DELETE,
-            replacements: [StaffId, dept_id,1,1]
+            replacements: [user.staff_id, dept_id,1,1]
         });
     }
 
+    // insert groups
     for (const dept_id of GroupsAdd) {
-        await sequelizeHost.query(sql_insert_roles, {
+        await sequelizeHost.query(sql.insert_roles, {
             type: QueryTypes.INSERT,
-            replacements: [StaffId, dept_id,1,1]
+            replacements: [user.staff_id, dept_id,1,1]
         });
     }
 
     response.send({message: "OK",
-                         usercreated: userCreated,
-                         password: NewUser.pwd});
+                         usercreated: user.created,
+                         password: user.pwd});
 }
 
-async function createUser(Request : RequestData): Promise<UserCreation>{
+async function createUser(Request : RequestData): Promise<User> {
 
-    type StaffId={
-        staff_id: number
-    }
+    let user: User = {
+        username: "",
+        isactive: 0,
+        pwd: "",
+        created: false,
+        staff_id: 0
+    };
 
-    const sql_insert_user: string = `INSERT INTO ost_staff (dept_id, role_id, username, firstname, lastname,email, phone, mobile, signature, created, updated, passwd, change_passwd ) 
-                                                    VALUES (?,"1",?,?,?,?,"","","",NOW(),NOW(),md5(?),"1")`;
-
-
-    const sql_find_user: string = `SELECT staff_id FROM ost_staff WHERE username=?`;
-
+    // gen init pwd
     let crypto = require("crypto");
     let pwd = crypto.randomBytes(10).toString('hex');
 
-    await sequelizeHost.query(sql_insert_user, {
+    // insert user into osticket table
+    await sequelizeHost.query(sql.create_user, {
         type: QueryTypes.INSERT,
         replacements: [Config.DEFAULT_DEPT_ID, "v".concat(Request.user_id), Request.firstname, Request.lastname, Request.email, pwd]
     });
 
-    const StaffId: StaffId[] = await sequelizeHost.query(sql_find_user, {
+    // check if user creation successful and get additional fields
+    const users: User[] = await sequelizeHost.query(sql.find_user, {
         type: QueryTypes.SELECT,
         replacements: ["v".concat(Request.user_id)],
     });
 
-    if (StaffId.length != 0) {
-        return {staff_id: StaffId[0].staff_id, pwd: pwd };
-    }else{
-        return {staff_id: 0, pwd: "" };
+    if (users.length != 0) {
+        let user: User = users[0];
+        user.created = true;
+        user.pwd = pwd;
     }
+    return user;
 }
 export default {
     syncUserGroups,
---
+++ src/utility/SQLStatements.ts
@@ -0,0 +1,29 @@
+
+const sql_find_user: string = `SELECT *
+                               FROM ost_staff
+                               WHERE username = ?`;
+const sql_find_groups: string = `SELECT dept_id
+                                 FROM ost_staff_dept_access
+                                 WHERE staff_id = ?`;
+const sql_delete_roles: string = `DELETE
+                                  FROM ost_staff_dept_access
+                                  WHERE staff_id = ?
+                                    and dept_id = ?`;
+const sql_insert_roles: string = `INSERT INTO ost_staff_dept_access (staff_id, dept_id, role_id, flags)
+                                  VALUES (?, ?, ?, ?)`;
+
+const sql_create_user: string = `INSERT INTO ost_staff (dept_id, role_id, username, firstname, lastname,email, phone, mobile, signature, created, updated, passwd, change_passwd ) 
+                                                    VALUES (?,"1",?,?,?,?,"","","",NOW(),NOW(),md5(?),"1")`;
+
+const sql_user_switch_active: string = `UPDATE ost_staff SET isactive = ? WHERE staff_id = ?`;
+
+const sql = {find_user : sql_find_user,
+                                        find_groups: sql_find_groups,
+                                        delete_roles: sql_delete_roles,
+                                        insert_roles: sql_insert_roles,
+                                        create_user: sql_create_user,
+                                        switch_active: sql_user_switch_active}
+
+export  {
+    sql
+}
